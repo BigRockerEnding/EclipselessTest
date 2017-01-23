c1fc90d66396c1c9c24d12432b1b2e618b36faf7
League <league@WorthyMini-7.attlocal.net>
2017-01-22 17:34:22 -0800

    Initial Commit

---
+++ src/org/jointheleague/stephenh/test/TestClass.java
@@ -0,0 +1,10 @@
+package org.jointheleague.stephenh.test;
+
+class TestClass {
+
+    public static void main(String... args) {
+	System.out.println("This is eclipseless, and this isn't madness");
+	System.out.println("This isn't sparta either");
+    }
+
+}
